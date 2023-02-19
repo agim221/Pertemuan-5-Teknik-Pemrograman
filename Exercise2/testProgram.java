8274652d96a10670fbf17e23363d2591b3a21c3c
agim221 <108734183+agim221@users.noreply.github.com>
2023-02-19 09:46:34 +0700

    Add files via upload

---
+++ Exercise2/testProgram.java
@@ -0,0 +1,13 @@
+package Exercise2;
+
+public class testProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s1 = new Shape();
+		Circle c1 = new Circle();
+		System.out.println(s1.toString());
+		System.out.println(c1.toString());
+	}
+
+}
---
+++ excercise3/Sortable.java
@@ -0,0 +1,8 @@
+package excercise3;
+
+public interface Sortable{
+	public abstract int compare(Sortable b);
+	public static void shell_sort(Sortable[] a) {
+		//shell sort body
+	}
+}
